5ca51ed87f0edab3a6f056ce9351e31efe2f6bb3
atzisb-kozak <alkergoat@gmail.com>
2021-09-30 08:43:39 +0200

    [Docs](delay) add comment to delay function

--- src/delay.ts
+++ src/delay.ts
@@ -2,6 +2,14 @@ import { timeString } from '@constant/time'
 
 type TimeUnit = 'seconde' | 'minute' | 'heure';
 
-export function delay(time: number, unit: TimeUnit = 'seconde') {
+/**
+ * awaiting timed function, use to wait between 2 process
+ *
+ * @export
+ * @param {number} time Time time to wait 
+ * @param {TimeUnit} [unit='seconde'] Time unit (second, hours, minute)
+ * @return {*}  {Promise<NodeJS.Timeout>}
+ */
+export function delay(time: number, unit: TimeUnit = 'seconde'): Promise<NodeJS.Timeout> {
 	return Promise.resolve(setTimeout(() => {}, time * timeString[unit]));
 };
